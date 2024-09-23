be374e73cd0b8141f4ba2a761e90ddf1405ada01
Danilo Santos <dscofield@outlook.com.br>
2024-09-23 19:57:10 -0300

    feat: add GetUserByEmailRepository

---
+++ src/repositories/postgres/user/get-user-by-email.js
@@ -0,0 +1,11 @@
+import { prisma } from '../../../../prisma/prisma.js';
+
+export class GetUserByEmailRepository {
+    async execute(email) {
+        return prisma.user.findUnique({
+            where: {
+                email,
+            },
+        });
+    }
+}
