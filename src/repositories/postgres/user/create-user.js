8871844bd370c676bf530143cf2cb82226ee8df4
Danilo Santos <dscofield@outlook.com.br>
2024-09-23 19:01:31 -0300

    feat: add PostgresCreateUserRepository

---
+++ src/repositories/postgres/user/create-user.js
@@ -0,0 +1,15 @@
+import { prisma } from '../../../../prisma/prisma';
+
+export class PostgresCreateUserRepository {
+    async execute(createUserParams) {
+        return await prisma.user.create({
+            data: {
+                id: createUserParams.id,
+                first_name: createUserParams.first_name,
+                last_name: createUserParams.last_name,
+                email: createUserParams.email,
+                password: createUserParams.password,
+            },
+        });
+    }
+}
