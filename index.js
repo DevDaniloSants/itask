f4d3e96bad1856408f02d57a2fb381ec8198a1db
Danilo Santos <dscofield@outlook.com.br>
2024-09-23 19:50:30 -0300

    feat: add POST route for creating a user

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 import express from 'express';
 import 'dotenv/config';
+import { CreateUserController } from './src/controllers/index.js';
+import { PostgresCreateUserRepository } from './src/repositories/index.js';
+import { CreateUserUseCase } from './src/use-cases/index.js';
 
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.post('/', async (request, response) => {
+    const createUserRepository = new PostgresCreateUserRepository();
+    const createUserUseCase = new CreateUserUseCase(createUserRepository);
+    const createUserController = new CreateUserController(createUserUseCase);
+
+    const { statusCode, body } = await createUserController.execute(request);
+
+    response.status(statusCode).send(body);
 });
 
 app.listen(port, () => {
