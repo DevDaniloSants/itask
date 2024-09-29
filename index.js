fec3d968a3754acc6207459f7e68d0537e33de7f
Danilo Santos <dscofield@outlook.com.br>
2024-09-29 11:47:08 -0300

    feat: add Implement GetUserById route with controller, use case, and repository

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import 'dotenv/config';
-import { CreateUserController } from './src/controllers/index.js';
+import {
+    CreateUserController,
+    GetUserByIdController,
+} from './src/controllers/index.js';
+
 import {
     GetUserByEmailRepository,
+    GetUserByIdRepository,
     PostgresCreateUserRepository,
 } from './src/repositories/index.js';
-import { CreateUserUseCase } from './src/use-cases/index.js';
+
+import {
+    CreateUserUseCase,
+    GetUserByIdUseCase,
+} from './src/use-cases/index.js';
 
 const app = express();
 const port = process.env.PORT;
@@ -26,6 +35,16 @@ app.post('/', async (request, response) => {
     const createUserController = new CreateUserController(createUserUseCase);
 
     const { statusCode, body } = await createUserController.execute(request);
+
+    response.status(statusCode).send(body);
+});
+
+app.get('/:id', async (request, response) => {
+    const getUserByIdRepository = new GetUserByIdRepository();
+    const getUserByIdUseCase = new GetUserByIdUseCase(getUserByIdRepository);
+    const getUserByIdController = new GetUserByIdController(getUserByIdUseCase);
+
+    const { statusCode, body } = await getUserByIdController.execute(request);
 
     response.status(statusCode).send(body);
 });
