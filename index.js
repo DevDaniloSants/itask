25f1413abf12fac80873135484975c03b612de12
Danilo Santos <dscofield@outlook.com.br>
2024-10-11 16:02:53 -0300

    feat: add DeleteUser route

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import 'dotenv/config';
 import {
     CreateUserController,
+    DeleteUserController,
     GetUserByIdController,
 } from './src/controllers/index.js';
 
 import {
+    DeleteUserRepository,
     GetUserByEmailRepository,
     GetUserByIdRepository,
     PostgresCreateUserRepository,
@@ -13,6 +15,7 @@ import {
 
 import {
     CreateUserUseCase,
+    DeleteUserUseCase,
     GetUserByIdUseCase,
 } from './src/use-cases/index.js';
 
@@ -21,11 +24,7 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.post('/', async (request, response) => {
+app.post('/api/users/', async (request, response) => {
     const createUserRepository = new PostgresCreateUserRepository();
     const getUserByEmailRepository = new GetUserByEmailRepository();
     const createUserUseCase = new CreateUserUseCase(
@@ -39,12 +38,22 @@ app.post('/', async (request, response) => {
     response.status(statusCode).send(body);
 });
 
-app.get('/:id', async (request, response) => {
+app.get('/api/users/:id', async (request, response) => {
     const getUserByIdRepository = new GetUserByIdRepository();
     const getUserByIdUseCase = new GetUserByIdUseCase(getUserByIdRepository);
     const getUserByIdController = new GetUserByIdController(getUserByIdUseCase);
 
     const { statusCode, body } = await getUserByIdController.execute(request);
+
+    response.status(statusCode).send(body);
+});
+
+app.delete('/api/users/:userId', async (request, response) => {
+    const deleteUserRepository = new DeleteUserRepository();
+    const deleteUserUseCase = new DeleteUserUseCase(deleteUserRepository);
+    const deleteUserController = new DeleteUserController(deleteUserUseCase);
+
+    const { statusCode, body } = await deleteUserController.execute(request);
 
     response.status(statusCode).send(body);
 });
